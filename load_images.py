4c9336532ec2230e9605aeb478ba64e5bbdb041b
LinesPrower <linesprower@gmail.com>
2018-11-28 11:25:08 +0500

    Refactoring

--- load_images.py
+++ load_images.py
@@ -18,38 +18,31 @@ def pickle_images_labels():
 			images_labels.append((np.array(img, dtype=np.uint8), int(g_id)))
 	return images_labels
 
-def split_images_labels(images_labels):
-	images = []
-	labels = []
-	for (image, label) in images_labels:
-		images.append(image)
-		labels.append(label)
-	return images, labels
-
 images_labels = pickle_images_labels()
-images_labels = shuffle(shuffle(shuffle(shuffle(images_labels))))
-images, labels = split_images_labels(images_labels)
+images_labels = shuffle(images_labels)
+images, labels = zip(*images_labels) 
 print("Length of images_labels", len(images_labels))
 
-train_images = images[:int(5/6*len(images))]
+train_images_count = int(5/6*len(images))
+train_images = images[:train_images_count]
 print("Length of train_images", len(train_images))
 with open("train_images", "wb") as f:
 	pickle.dump(train_images, f)
 del train_images
 
-train_labels = labels[:int(5/6*len(labels))]
+train_labels = labels[:train_images_count]
 print("Length of train_labels", len(train_labels))
 with open("train_labels", "wb") as f:
 	pickle.dump(train_labels, f)
 del train_labels
 
-test_images = images[int(5/6*len(images)):]
+test_images = images[train_images_count:]
 print("Length of test_images", len(test_images))
 with open("test_images", "wb") as f:
 	pickle.dump(test_images, f)
 del test_images
 
-test_labels = labels[int(5/6*len(labels)):]
+test_labels = labels[train_images_count:]
 print("Length of test_labels", len(test_labels))
 with open("test_labels", "wb") as f:
 	pickle.dump(test_labels, f)
